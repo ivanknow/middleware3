2d0ccc3e1ff04212a479f311dfcf4fb4a29bb505
jpaulosilva <jpaulo_silva@yahoo.com.br>
2015-10-30 16:25:47 -0300

    ServerEncriptaDesencriptaRun

--- src/negocio/ServerEncriptaDesencriptaRun.java
+++ src/negocio/ServerEncriptaDesencriptaRun.java
@@ -3,48 +3,55 @@ package negocio;
 import middware.Comm;
 
 public class ServerEncriptaDesencriptaRun {
-	
-public static void main(String[] args) throws Exception {
-		
+
+	public static void main(String[] args) throws Exception {
+
 		ServerEncriptaDesencripta servico = new ServerEncriptaDesencripta();
-		RegistroServidor rs = new RegistroServidor("localhost","5553");
-		
+		RegistroServidor rs = new RegistroServidor("localhost", "5553");
+
 		rs.setNomeServico(servico.getClass().getSimpleName());
-		
+
 		ServidorNomes sn = new ServidorNomes();
-		
+
 		Comm mCliente = new Comm(new RegistroServidor("localhost", "5000"));
 		Comm mServidor = new Comm(rs);
-		
-		Message reqMsg = new Message();
-		Message resMsg = new Message();
-		
-		
-		reqMsg.setValores(rs);
-		
-		reqMsg.setOperacao("adicionarServico");
-		
-		
-		mCliente.requestAndReceive(reqMsg);
-		
-		
-		while (true) {
-			reqMsg = mServidor.receive();
-			String op = reqMsg.getOperacao();
-			switch (op) {
-			case "encripta":
-				resMsg.setValores(servico.encripta((String)reqMsg.getValores()));
-				break;
-				
-			case "desencripta":
-				CrypWrap cw = (CrypWrap)reqMsg.getValores();
-				resMsg.setValores(servico.desencripta(cw.chave,cw.textoEncriptado));
-			}
 
-			mServidor.reply(resMsg);
+		Message reqMsg = new Message();
+
+		reqMsg.setValores(rs);
+
+		reqMsg.setOperacao("adicionarServico");
+
+		mCliente.requestAndReceive(reqMsg);
+
+		while (true) {
+
+			MiddlewareThread thread = new MiddlewareThread(mServidor.receiveThread()) {
+
+				@Override
+				public Message exec(Message mIn) {
+					Message mOut = new Message();
+					ServerEncriptaDesencripta servico = new ServerEncriptaDesencripta();
+					String op = mIn.getOperacao();
+
+					switch (op) {
+					case "encripta":
+						mOut.setValores(servico.encripta((String) mIn.getValores()));
+						break;
+					case "desencripta":
+						CrypWrap cw = (CrypWrap) mIn.getValores();
+						mOut.setValores(servico.desencripta(cw.chave,cw.textoEncriptado));
+						break;
+					default:
+						break;
+					}
+					return mOut;
+				}
+			};
+			new Thread(thread).start();
 
 		}
-		
+
 	}
 
 }
